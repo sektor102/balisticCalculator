892ff16faa3bc2d82b30fea7da2068d8fb7abdf8
sektor102 <dante102@mail.ru>
2026-09-11 23:20:31 +0300

    Bump PWA cache after iPhone input fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wardogs-fast-fire-v1';
+const CACHE = 'wardogs-fast-fire-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
@@ -11,7 +11,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(fetch(event.request).then(response => {
-    const copy=response.clone();
+    const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy));
     return response;
   }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html'))));
