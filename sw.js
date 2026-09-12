491594ee4229f7c0e151f12acfacaf64e6c0c6f4
sektor102 <dante102@mail.ru>
2026-09-12 03:23:50 +0300

    Update service worker for OCR and cache refresh

--- sw.js
+++ sw.js
@@ -1,18 +1,47 @@
-const CACHE = 'wardogs-fast-fire-v3';
+const CACHE = 'wardogs-fast-fire-v5';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).then(response => {
-    const copy=response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
-    return response;
-  }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html'))));
+
+  const url = new URL(event.request.url);
+
+  // Do not intercept OCR/CDN/model requests. If they fail, returning index.html
+  // would break WebAssembly/worker loading.
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
